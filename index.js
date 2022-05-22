8757a1ae62ef931944af27a43348ff3f44deca0f
ilakkiya123 <98203967+ilakkiya123@users.noreply.github.com>
2022-05-22 13:10:41 +0530

    Create index.js
    js file added

---
+++ index.js
@@ -0,0 +1,8 @@
+const breweryURL="https://api.openbrewerydb.org/breweries?by_name=cooper&per_page=3"
+
+const getBrewery = async () => {
+  const cityName = document.getElementById("text-inp").value;
+  const response = await fetch(`${breweryURL}?by_name=${names}&per_pages=3`);
+  const data = await response.json();
+  console.log(data);
+}
